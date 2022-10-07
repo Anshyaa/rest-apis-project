8d1263ac4a020074645ed45dab5f0eaaaff4a3ba
Anshyaa <110979027+Anshyaa@users.noreply.github.com>
2022-10-07 15:45:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
 
 
 from resources.item import blp as ItemBlueprint
@@ -17,6 +18,7 @@ import os
 #register the blieprint wuth the api
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
