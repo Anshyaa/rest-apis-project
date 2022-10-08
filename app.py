e1d3b9183b0ac86807c0b55e273e824e48679fa8
Anshyaa <110979027+Anshyaa@users.noreply.github.com>
2022-10-08 16:11:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "postgres://bneqftur:sfYV3MvWSvZmHX6nkWeRH633c3uq-f_N@lucky.db.elephantsql.com/bneqftur")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate = Migrate(app, db)
